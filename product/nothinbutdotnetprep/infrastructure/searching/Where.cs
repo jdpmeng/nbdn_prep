5a97bb9b9fdf52ec89138b7be8b4cd7a68448998
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-12-06 20:55:56 -0500

    Working on not equal to

--- product/nothinbutdotnetprep/infrastructure/searching/Where.cs
+++ product/nothinbutdotnetprep/infrastructure/searching/Where.cs
@@ -1,13 +1,37 @@
 using System;
+using System.Collections.Generic;
+using nothinbutdotnetprep.collections;
 
 namespace nothinbutdotnetprep.infrastructure.searching
 {
-    public class Where<ElementToFind>
+    public class Where<ItemToFilter>
     {
-        public static Func<ElementToFind, PropertyType> has_a<PropertyType>(
-            Func<ElementToFind, PropertyType> property_accessor)
+        public static CriteriaFactory<ItemToFilter,PropertyType> has_a<PropertyType>(
+            Func<ItemToFilter, PropertyType> property_accessor)
         {
-            return property_accessor;
+            return new CriteriaFactory<ItemToFilter, PropertyType>(property_accessor);
+        }
+    }
+
+    public class CriteriaFactory<ItemToFilter,PropertyType>
+    {
+        Func<ItemToFilter,PropertyType> property_accessor;
+
+        public CriteriaFactory(Func<ItemToFilter, PropertyType> property_accessor)
+        {
+            this.property_accessor = property_accessor;
+        }
+
+        public Criteria<ItemToFilter> equal_to(PropertyType value_to_equal)
+        {
+            return new AnonymousCriteria<ItemToFilter>(x => property_accessor(x).Equals(value_to_equal));
+        }
+
+        public Criteria<ItemToFilter> equal_to_any(params PropertyType[] values)
+        {
+            return
+                new AnonymousCriteria<ItemToFilter>(x => new List<PropertyType>(values).Contains(property_accessor(x)));
+
         }
     }
 }
